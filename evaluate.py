b5117738443112280b74e7f057b85dd78b0af73b
muhammetfaik <muhammetfaik@gmail.com>
2020-03-05 16:20:51 +0300

    debugger add

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,8 @@ from utils import CharacterTable, transform
 from utils import restore_model, decode_sequences
 from utils import read_text, tokenize
 
+import pdb
+
 error_rate = 0.6
 reverse = True
 model_path = './models/seq2seq.h5'
@@ -26,24 +28,38 @@ if __name__ == '__main__':
     train_encoder, train_decoder, train_target = transform(
         vocab, maxlen, error_rate=error_rate, shuffle=False)
 
+    pdb.set_trace()
+
     tokens = tokenize(test_sentence)
     tokens = list(filter(None, tokens))
     nb_tokens = len(tokens)
     misspelled_tokens, _, target_tokens = transform(
         tokens, maxlen, error_rate=error_rate, shuffle=False)
 
+    pdb.set_trace()
+
+
     input_chars = set(' '.join(train_encoder))
     target_chars = set(' '.join(train_decoder))
     input_ctable = CharacterTable(input_chars)
     target_ctable = CharacterTable(target_chars)
     
+    pdb.set_trace()
+
+
     encoder_model, decoder_model = restore_model(model_path, hidden_size)
     
+    pdb.set_trace()
+
+
     input_tokens, target_tokens, decoded_tokens = decode_sequences(
         misspelled_tokens, target_tokens, input_ctable, target_ctable,
         maxlen, reverse, encoder_model, decoder_model, nb_tokens,
         sample_mode=sample_mode, random=False)
     
+    pdb.set_trace()
+
+
     print('-')
     print('Input sentence:  ', ' '.join([token for token in input_tokens]))
     print('-')
